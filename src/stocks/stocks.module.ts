be95cf7dd4f60c1ca820e5ce26b589b419020982
Kushwanth <nethikushwanth@gmail.com>
2026-01-28 15:40:03 +0530

    Update stocks module and main entry

--- src/stocks/stocks.module.ts
+++ src/stocks/stocks.module.ts
@@ -3,15 +3,12 @@ import { StocksService } from './stocks.service';
 import { StocksController } from './stocks.controller';
 import { StockOfTheWeekService } from './stock-of-the-week.service';
 import { StockOfTheWeekController } from './stock-of-the-week.controller';
-import { ScreenerService } from './screener.service';
-import { ScreenerController } from './screener.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [StocksController, StockOfTheWeekController, ScreenerController],
-  providers: [StocksService, StockOfTheWeekService, ScreenerService],
+  controllers: [StocksController, StockOfTheWeekController],
+  providers: [StocksService, StockOfTheWeekService],
 })
 export class StocksModule { }
-
 
